8a8508dd807009d9af278e363458caede2740981
Andrew Hudson <a.j.hudson@hotmail.com>
2021-03-10 16:30:02 +1100

    simplified bash backup/restore script

--- bash/docker_volume_backup.sh
+++ bash/docker_volume_backup.sh
@@ -20,6 +20,7 @@ backup() {
     echo "Successfully backed up volume $vol_src to ${local_bkp}/${cont_bkp_file}"
 }
 
+# FIXME: need to find the latest backup automatically. Using an auto timestamp dont work for restores.
 restore() {
     local vol_src=$1
     local vol_tgt=$2
@@ -27,7 +28,6 @@ restore() {
     local cont_bkp_dir=$4
     local cont_bkp_file=$5
     local img=$6
-    local strip=$7
     
     echo "Restoring volume: $vol_src"
 
@@ -37,20 +37,19 @@ restore() {
         --mount source=${vol_src},target=${vol_tgt} \
         -v ${local_bkp}:${cont_bkp_dir} \
         $img tar -C $vol_tgt -xvf ${cont_bkp_dir}/${cont_bkp_file}
-        #$img tar -C $vol_tgt -xvf ${cont_bkp_dir}/${cont_bkp_file} --strip=${strip}
 
     echo "Successfully restored volume $vol_src from ${local_bkp}/${cont_bkp_file}"
 }
 
-#FIXME: more control over whether to force a restore to existing volume
 preflight() {
     local action=$1
     local vol_src=$2
+    local backup_file=$3
 
     case $action in
         'backup')
             if ! docker volume inspect $vol_src > /dev/null 2>&1; then
-                echo "cant find source volume: $vol_src"
+                echo "[ERROR] cant find source volume: $vol_src"
                 exit 1
             fi
             ;;
@@ -59,33 +58,45 @@ preflight() {
                 echo "Creating source volume:"
                 docker volume create $vol_src
             else
-                echo "Docker volume $vol_src already exists"
+                echo "[ERROR] Docker volume $vol_src already exists"
                 exit 1
             fi
             ;;
         *)
-            echo "Unknown action: $action"
+            echo "[ERROR] Unknown action: $action"
             exit 1
             ;;
     esac
+
+    ext=$(echo "$backup_file" | awk -F . '{print $NF}')
+    if [[ $ext != 'tar' ]]; then
+        echo "[ERROR] Make sure you specify a .tar file for backup and restores. File specified: $backup_file"
+        exit 1
+    fi
+
 }
 
-#TODO: fix usage and example lines
+# TODO: 
+#   option: force a restore to existing volume
+#   option: specify file to backup to/restore from
+# FIXME: forcing .tar backups is shit
 usage() {
-    echo "usage: ${0} -a [backup||restore]"
-    echo "  -a <action> Backup or restore a docker volume"
-    echo "  -s <volume source> The volume to backup or restore"
-    echo "  -t <volume target> The directory in the container to mount volume source on"
-    echo "  -l <local backup dir> Host directory to bind mount"
-    echo "  -d <container backup dir> Container directory to bind to local host"
-    echo "  -f <container backup file> File to backup to or restore from in container"
-    echo "  -h <help> Display this message"
-    echo "example: ${0} -a backup"
+    echo "Usage: ${0} -a action -v docker_volume -b backup_directory"
+    echo ""
+    echo "  Use this script to create tar backups of your docker volumes"
+    echo ""
+    echo "Options:"
+    echo "  -a backup|restore   Action: Create a backup or restore from an existing backup       (required)"
+    echo "  -v TEXT             Docker volume: Name of docker volume to backup or restore from   (required)"
+    echo "  -b TEXT             Backup file: Absolute path to backup tar file                    (required)"
+    echo "  -h                  Help: Displays this message"
+    echo ""
+    echo "example: ${0} -a backup -v my_volume -b /home/docker_volume_backups/backup.tar"
     exit 1
 }
 
-#Main
-while getopts "a:s:t:l:d:f:h" opt; do
+# Main
+while getopts "a:v:b:h" opt; do
     case $opt in
         'a')
             if [[ $OPTARG =~ ^(backup|restore)$ ]]; then
@@ -95,20 +106,11 @@ while getopts "a:s:t:l:d:f:h" opt; do
                 usage
             fi
             ;;
-        's')
+        'v')
             vol_src=${OPTARG}
             ;;
-        't')
-            vol_tgt=${OPTARG}
-            ;;
-        'l')
+        'b')
             local_bkp=${OPTARG}
-            ;;
-        'd')
-            cont_bkp_dir=${OPTARG}
-            ;;
-        'f')
-            cont_bkp_file=${OPTARG}
             ;;
         'h')
             usage
@@ -120,14 +122,19 @@ while getopts "a:s:t:l:d:f:h" opt; do
     esac
 done
 
-if [[ ! -n $action || ! -n $vol_src || ! -n $vol_tgt || ! -n $local_bkp || ! -n $cont_bkp_dir || ! -n $cont_bkp_file ]]; then
+if [[ ! -n $action || ! -n $vol_src || ! -n $local_bkp ]]; then
     echo "Missing a required parameter"
     usage
 fi
 
-#TODO: add image as an option
+# Global vars
+vol_tgt='/docker_vol'
+cont_bkp_dir='/backups'
 img='alpine'
+cont_bkp_file=$(basename ${local_bkp})
+local_bkp_dir=$(dirname ${local_bkp})
 
-preflight $action $vol_src
+# Validate pre-flight checks before proceeding
+preflight $action $vol_src $cont_bkp_file
 
-$action $vol_src $vol_tgt $local_bkp $cont_bkp_dir $cont_bkp_file $img
+$action $vol_src $vol_tgt $local_bkp_dir $cont_bkp_dir $cont_bkp_file $img
--- bash/test.sh
+++ bash/test.sh
@@ -1,22 +1,62 @@
 #!/bin/bash
 
-#TODO: write proper test case
+backup_test() {
+    local test_vol=$1
 
-#docker run -it --rm --mount source=test_vol,target=/tmp/code alpine sh -c "echo 'Hello, World!' > /tmp/code/test_file"
-#docker run --rm --mount source=test_vol,target=/tmp/code alpine cat /tmp/code/test_file
+    # Create a test volume add some dummy data to it
+    docker run \
+        -it \
+        --rm \
+        --mount source=${test_vol},target=/tmp/code \
+        alpine sh -c "echo 'Hello, World!' > /tmp/code/test_file"
 
-# ./docker_volume_backup.sh \
-#     -a backup \
-#     -s test_vol \
-#     -t /tmp/code \
-#     -l /Users/ahudson/Code/github/saccy/docker_vol_backup/test \
-#     -d /backup/test \
-#     -f backup.tar
+    # Backup the test volume
+    ./docker_volume_backup.sh \
+        -a backup \
+        -v ${test_vol} \
+        -b /Users/andrew/Code/pg_dumper/${test_vol}.tar
 
-./docker_volume_backup.sh \
-    -a restore \
-    -s test_vol \
-    -t /tmp/code \
-    -l /Users/ahudson/Code/github/saccy/docker_vol_backup/test \
-    -d /backup/test \
-    -f backup.tar
+    tar -xvzf "/Users/andrew/Code/pg_dumper/${test_vol}.tar"
+    test_out="$(cat ./test_file)"
+
+    docker volume rm $test_vol > /dev/null
+
+    if [[ $test_out == 'Hello, World!' ]]; then
+        echo "[SUCCESS] Backup test successful"
+        exit 0
+    else
+        echo "[ERROR] Backup test failed"
+        exit 1
+    fi
+
+}
+
+restore_test() {
+    local test_vol=$1
+
+    # Create a new docker volume from an existing backup file
+    ./docker_volume_backup.sh \
+        -a restore \
+        -v ${test_vol} \
+        -b /Users/andrew/Code/pg_dumper/${test_vol}.tar
+
+    test_out="$(docker run --rm --mount source=${test_vol},target=/tmp/code alpine cat /tmp/code/test_file)"
+
+    docker volume rm $test_vol > /dev/null
+
+    if [[ $test_out == 'Hello, World!' ]]; then
+        echo "[SUCCESS] Restore test successful"
+        exit 0
+    else
+        echo "[ERROR] Restore test failed"
+        exit 1
+    fi
+
+}
+
+# Main
+action=$1 # backup|restore
+test_vol='test_volume'
+
+# backup_test $test_vol
+${action}_test $test_vol
